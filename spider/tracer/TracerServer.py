2b976f53243cb2ddcff5c3db32f55ce67e3936f4
yang han <hanyang213@gmail.com>
2012-06-05 00:25:12 +0800

    add web interface for new hot topic weibo mid && filter repeat mids in queues

---
+++ spider/tracer/TracerServer.py
@@ -0,0 +1,30 @@
+#-*-coding=utf-8-*-
+
+
+import web
+import json
+from queues import MidQueue
+
+
+urls = (
+    '/htopic/(.*)', 'HotTopic',
+)
+job_list = MidQueue()
+
+
+class HotTopic:
+    def GET(self, string):
+        mid = info = None
+        try:
+            mid, info = string.split('@')
+        except:
+            pass
+        if mid and info:
+            job_list.push(mid, info)
+            return json.dumps({'status': 'ok'})
+        return json.dumps({'status': 'error'})
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
+            
