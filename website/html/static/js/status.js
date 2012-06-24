44dc6a3f35498b87920bebe0bccc2c377c06cb0e
yang han <hanyang213@gmail.com>
2012-06-24 16:17:41 +0800

    fixes #2

--- website/html/static/js/status.js
+++ website/html/static/js/status.js
@@ -33,6 +33,7 @@ function drawGraph(){
 	type: "GET",
 	success: function(data){
 	    //console.log(data-count)
+	    data = String(data)
             total_count = data.split(';')[0];
             target_count = data.split(';')[1]; 
 	    $("#total_count").html('抓取总数:'+total_count);
@@ -58,6 +59,7 @@ $.ajax({
     url: "/count/",
     type: "GET",
     success: function(data){
+        data = String(data)
 	total_count = data.split(';')[0];
 	target_count = data.split(';')[1];
 	count = total_count;
