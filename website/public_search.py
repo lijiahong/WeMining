e1aaa643e644939a2fa09b3674b3151a40e59eff
hanyangrabbit <hanyang213@gmail.com>
2012-07-14 13:12:00 +0800

    update search

--- website/public_search.py
+++ website/public_search.py
@@ -10,24 +10,31 @@ urls = ('/api/public/search.json', )
 
 class handler():
     def GET(self):
-        form = web.input(q=None,t=None)
+        form = web.input(q=None,t=None, start=None, end=None, page=None)
         search = Search()
         keywords = cut(form.q)
-        return json.dumps(search.query(keywords, all=True if form.t else False))
+        args = {}
+        if form.start:
+            args['ts'] = {'$gte': form.start}
+        if form.end:
+            args['ts'] = {'$lte': form.end}
+        if not form.page:
+            form.page = 1
+        return json.dumps(search.query(keywords, all=True if not form.t else False, page=form.page, **args))
 
 
 class Search(object):
     def __init__(self):
         self.db = getDB()
     
-    def query(self, keywords, all=True, **kw):
+    def query(self, keywords, all=True, page=1, **kw):
         query_dict = {}
         query_dict.update(kw)
         if all:
             query_dict['_keywords'] =  {'$all': keywords}
         else:
             query_dict['_keywords'] =  {'$in': keywords}
-        results = self.db['public_statuses'].find(query_dict, sort=[('ts', pymongo.ASCENDING)])
+        results = self.db['public_statuses'].find(query_dict, sort=[('ts', pymongo.ASCENDING)]).skip((page-1)*20).limit(20)
         return list(results)
 
                  
