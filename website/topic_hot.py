848a68c18a810c8e3b466a8bf77329d64507eefb
hanyangrabbit <hanyang213@gmail.com>
2012-07-14 13:49:56 +0800

    add topic

---
+++ website/topic_hot.py
@@ -0,0 +1,24 @@
+#-*- coding:utf-8 -*-
+
+import json
+import web
+import time
+import pymongo
+
+from config import getDB
+
+db = getDB()
+urls = ('/api/topic/hot.json', )
+
+class handler():
+    def GET(self):
+        form = web.input(type=None,start=None, end=None)
+        if not form.start:
+            form.start = 0
+        if not form.end:
+            form.end = time.time()
+        query_dict = {}
+        query_dict['ts'] = {'$gte': start, '$lte': end}
+        if type:
+            query_dict['type'] = type
+        return json.dumps(list(db['topics'].find(query_dict, sort=[('ts', pymongo.ASCENDING)])))
