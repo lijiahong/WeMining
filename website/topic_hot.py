c89371ccdbee8eedc7b22ea170e361c953b5e12e
hanyangrabbit <hanyang213@gmail.com>
2012-07-14 13:54:12 +0800

    update topic

--- website/topic_hot.py
+++ website/topic_hot.py
@@ -18,7 +18,7 @@ class handler():
         if not form.end:
             form.end = time.time()
         query_dict = {}
-        query_dict['ts'] = {'$gte': form.start, '$lte': form.end}
+        query_dict['ts'] = {'$gte': int(form.start), '$lte': int(form.end)}
         if type:
             query_dict['type'] = form.type
         return json.dumps(list(db['topics'].find(query_dict, sort=[('ts', pymongo.ASCENDING)])))
