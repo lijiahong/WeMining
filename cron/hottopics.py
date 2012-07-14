40ab389781e1e99d869b35f018f27e0102a4ec27
hanyangrabbit <hanyang213@gmail.com>
2012-07-14 16:14:54 +0800

    add cron job for hot topics

---
+++ cron/hottopics.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import sys
+sys.path.append('..')
+
+from api.dev_api import APIClient
+from spider.config import getDB
+
+def topic(api, type=None):
+    if type == 'hour':
+        res = api.trends__hourly(source='2298231745',base_app=0)['trends']
+    elif type == 'day':
+        res = api.trends__daily(source='2298231745',base_app=0)['trends']
+    elif type == 'week':
+        res = api.trends__weekly(source='2298231745',base_app=0)['trends']
+    else:
+        return None
+    data = []
+    for key in res.keys():
+        for v in res[key]:
+            data.append({'topic': v['query'], 'count': v['amount']})
+    return data
+    
+
+def main():
+    try:
+        action = sys.argv[1]
+        api = APIClient('linhao1992@gmail.com', 'weibomap', '2298231745')
+        present_time = time.time()
+        data = topic(api, type=action)
+        db = getDB()
+        if data:
+             for d in data:
+                 db.topics.save({'topic': d['topic'],
+                                 'count': d['count'],
+                                 'ts':  present_time,
+                                 'type': action})
+    except IndexError:
+        print 'wrong argument.'
+             
+if __name__ == '__main__': main()
