b1a4f9b87ccb9e6d8e31f4b9a3db69c4fec470c7
hanyangrabbit <hanyang213@gmail.com>
2012-07-14 15:55:15 +0800

    fix tokenizer dict problem

--- tokenizer/fenci.py
+++ tokenizer/fenci.py
@@ -8,12 +8,17 @@ def cut(text,f=None):
     s.set_charset('utf-8')
     s.set_dict('/usr/local/scws/etc/dict.utf8.xdb',scws.XDICT_MEM)
     s.add_dict('/usr/local/scws/etc/dict_cht.utf8.xdb',scws.XDICT_MEM)
-    s.add_dict('userdic.txt',scws.XDICT_TXT)
+    s.add_dict('/opt/WeMining/tokenizer/userdic.txt',scws.XDICT_TXT)
     s.set_rules('/usr/local/scws/etc/rules.utf8.ini')
     s.set_ignore(1)
-    stopwords = set([line.strip('\r\n') for line in file('ext_stopword.dic')])
+    stopwords = set([line.strip('\r\n') for line in file('/opt/WeMining/tokenizer/ext_stopword.dic')])
     if f:
         return [token[0].decode('utf-8') for token in s.participle(text.encode('utf-8')) if token[0] not in stopwords and token[1] in f]
     else:
         return [token[0].decode('utf-8') for token in s.participle(text.encode('utf-8')) if token[0] not in stopwords]
-    
+
+def main():
+    for t in cut(u'德罗巴：申花应签下兰帕德和特里 来不为退役'):
+        print t
+
+if __name__ == '__main__': main()
