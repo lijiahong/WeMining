1797edea615305f6537ba33883d50075f9139f1d
hanyangrabbit <hanyang213@gmail.com>
2012-11-12 12:06:55 +0800

    update topic.

--- mining/topic.py
+++ mining/topic.py
@@ -3,8 +3,14 @@
 
 
 import time
-import xapian
+import json
+import operator
 import codecs
+
+import xapian
+
+def unix2local(ts):
+    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(ts))
 
 def date2ts(date):
     return time.mktime(time.strptime(date, '%Y-%m-%d'))
@@ -32,22 +38,100 @@ class Search(object):
         self.repnameslistvi = 9
         self.widvi = 10
 
-    def topic_query(self, begin=None, end=None, limit=5000000):
+    def topic_query(self, begin=None, end=None, limit=100000):
         self.qp.add_valuerangeprocessor(xapian.NumberValueRangeProcessor(self.timestampvi, ''))
         if begin and end:
-            timequerystr = str(begin) + '..' + str(end)
+            timequerystr = str(date2ts(begin)) + '..' + str(date2ts(end))
             timequery = self.qp.parse_query(timequerystr)
         else:
             return None
         self.enquire.set_query(timequery)
         self.enquire.set_sort_by_value(self.timestampvi, False)
         matches = self.enquire.get_mset(0, limit)
-        print matches.size()
+        ts_arr = []
+        results = []
+        total_keywords_count = {}
+        for match in matches:
+            result = {}
+            result['ts'] = xapian.sortable_unserialise(match.document.get_value(self.timestampvi))
+            result['keywords'] = list(set(json.loads(match.document.get_value(self.keywordsvi))))
+            for k in result['keywords']:
+                if k not in total_keywords_count:
+                    total_keywords_count[k] = 0
+                total_keywords_count[k] += 1
+            ts_arr.append(result['ts'])
+            results.append(result)
+        return ts_arr, results, total_keywords_count
+
+def partition(ts_arr, data, window_size=24*60*60):
+    ts_series = []
+    ts_start = ts_arr[0]
+    ts_end = ts_arr[-1]
+    each_step = window_size
+    ts_current = ts_start
+    data_cursor = -1
+    groups_keywords_count = []
+    while ts_current <= ts_end:
+        s_ts = ts_current
+        f_ts = ts_current + each_step
+        ts_series.append([s_ts, f_ts])
+        groups_size = []
+        group = []
+        for d in data[data_cursor+1:]:
+            ts = d['ts']
+            group.append(d)
+            if ts >= f_ts:
+                break
+            data_cursor += 1
+        groups_size.append(len(group))
+        if len(group):
+            group_keywords_count = {}
+            for d in group:
+                for k in d['keywords']:
+                    if k not in group_keywords_count:
+                        group_keywords_count[k] = 0
+                    group_keywords_count[k] += 1
+            groups_keywords_count.append(group_keywords_count)
+        ts_current += each_step
+    return ts_series, groups_keywords_count, groups_size
+
+def burst(ts_series, groups_keywords_count, total_keywords_count, groups_size, total_size):
+    word_burst_in_groups = []
+    for period, group_keywords_count, group_size in zip(ts_series, groups_keywords_count, groups_size):
+        word_burst_in_group = {}
+        for keyword in group_keywords_count.keys():
+            A = group_keywords_count[keyword]
+            B = total_keywords_count[keyword] - A
+            C = group_size - A
+            D = total_size - total_keywords_count[keyword] - C
+            try:
+                word_burst_in_group[keyword] = (A + B + C + D) * ((A*D - B*C) ** 2) * 1.0 / ((A + B) * (C + D) * (A + C) * (B + D))
+            except ZeroDivisionError:
+                word_burst_in_group[keyword] = 0
+        word_burst_in_groups.append(word_burst_in_group)
+    keywords_burst = {}
+    for keyword in total_keywords_count.keys():
+        for group in word_burst_in_groups:
+            if keyword in group:
+                if keyword not in keywords_burst:
+                    keywords_burst[keyword] = 0
+                keywords_burst[keyword] += group[keyword]
+    keywords_brust_list = sorted(keywords_burst.iteritems(), key=operator.itemgetter(1), reverse=True)
+    f = codecs.open('results.txt', 'w', encoding='utf-8')
+    for key, value in keywords_brust_list[:100]:
+        f.write('%s %s\n' % (key, value))
+    f.close()
+            
 
 def main():
-    time_start = date2ts('2012-9-1')
-    time_end = date2ts('2012-10-1')
+    time_start = '2012-9-1'
+    time_end = '2012-10-1'
     search = Search()
-    search.topic_query(begin=time_start, end=time_end)
+    ts_arr, results, total_keywords_count = search.topic_query(begin=time_start, end=time_end)
+    total_size = len(results)
+    print 'find %s statuses from %s to %s.' % (total_size, time_start, time_end)
+    ts_series, groups_keywords_count, groups_size = partition(ts_arr, results, window_size=24*60*60)
+    print 'partition ok.'
+    burst(ts_series, groups_keywords_count, total_keywords_count, groups_size, total_size)
 
 if __name__ == '__main__': main()
