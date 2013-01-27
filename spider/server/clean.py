6debd5325b04c135760a05f6ce4af1ac92565d16
linhaobuaa <1257819385@qq.com>
2013-01-28 03:28:03 +0800

    add database cleaning script

--- spider/server/clean.py
+++ spider/server/clean.py
@@ -36,13 +36,17 @@ def addNewUser():
 def cleanCollections():
     '''删除当前数据库中无用的集合 会阻塞数据库当前的其他进程
     '''
-    used = ['friendships', 'statuses', 'period', 'test', 'emotion_test', 'users', 'location', 'target_statuses']
+    used = ['friendships', 'statuses', 'period', 'test', 'emotion_test', 'users', 'location', 'target_statuses', 'april_user1', 'april_weibo1', 'user_statuses']
     for collection_name in db.collection_names():
         if collection_name in used:
             print collection_name, 'not remove'
+        print collection_name
         if collection_name not in used:
             print collection_name, 'removed'
             try:
                 db.drop_collection(collection_name)
-            except:
-                pass
+            except Exception, e:
+                print e
+
+
+if __name__ == '__main__': cleanCollections()
